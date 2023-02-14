77ca6468f41c4b2c46ab75a4bd30be9ce3844c47
francisjosephironman <francis.3.6.joseph@gmail.com>
2023-02-15 00:56:52 +0800

    added footer

--- articlelist-frontend/src/App.js
+++ articlelist-frontend/src/App.js
@@ -3,15 +3,23 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import Header from './components/Header/Header';
 import CreateFormModal from './components/CreateFormModal/CreateFormModal';
 import { useState } from 'react';
+import Footer from './components/Footer/Footer';
 
 function App() {
 
   const [isCreateFormModalShowing, setIsCreateFormModalShowing] = useState(false);
 
   return (
-    <div className="App">
+    <div className="App bg-secondary min-vh-100 d-flex flex-column justify-content-between">
       <Header onAddArticleButtonClick={() => setIsCreateFormModalShowing(true)} />
 
+
+
+
+
+      <Footer />
+
+      {/* modals */}
       <CreateFormModal 
         show={isCreateFormModalShowing} 
         onHide={() => setIsCreateFormModalShowing(false)} 
