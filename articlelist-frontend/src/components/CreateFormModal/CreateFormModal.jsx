e6da76a169c010be4f18aa842aaca458654da5b9
francisjosephironman <francis.3.6.joseph@gmail.com>
2023-02-15 00:44:14 +0800

    added create form modal

--- articlelist-frontend/src/components/CreateFormModal/CreateFormModal.jsx
+++ articlelist-frontend/src/components/CreateFormModal/CreateFormModal.jsx
@@ -1,7 +1,44 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { Form } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 function CreateFormModal(props) {
+
+  const [newArticle, setNewArticle] = useState({
+    description: "",
+    category: "",
+    keywords: "",
+    link: ""
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios.post('http://localhost:8080/api/articlelist/articles', newArticle)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => console.error(err));
+  }
+
+  const handleDescriptionChange = (e) => {
+    setNewArticle({...newArticle, description: e.target.value});
+  }
+
+  const handleCategoryChange = (e) => {
+    setNewArticle({...newArticle, category: e.target.value});
+  }
+
+  const handleKeywordsChange = (e) => {
+    setNewArticle({...newArticle, keywords: e.target.value});
+  }
+
+  const handleLinkChange = (e) => {
+    setNewArticle({...newArticle, link: e.target.value});
+  }
+
   return (
     <Modal
       {...props}
@@ -11,16 +48,51 @@ function CreateFormModal(props) {
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
+          Add Article
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className='mb-3'>
+            <Form.Label>Description</Form.Label>
+            <Form.Control
+              onChange={handleDescriptionChange}
+              type='text'
+              placeholder='e.g. What makes an API powerful.'
+            />
+          </Form.Group>
+
+          <Form.Group className='mb-3'>
+            <Form.Label>Category</Form.Label>
+            <Form.Control
+              onChange={handleCategoryChange}
+              type='text'
+              placeholder='e.g. Back-end'
+            />
+          </Form.Group>
+
+          <Form.Group className='mb-3'>
+            <Form.Label>Keywords</Form.Label>
+            <Form.Control
+              onChange={handleKeywordsChange}
+              type='text'
+              placeholder='e.g. Spring Java API'
+            />
+          </Form.Group>
+
+          <Form.Group className='mb-3'>
+            <Form.Label>Link</Form.Label>
+            <Form.Control
+              onChange={handleLinkChange}
+              type='text'
+              placeholder='e.g. example.com'
+            />
+          </Form.Group>
+
+          <Button variant='success' type='submit'>
+            Submit
+          </Button>
+        </Form>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
