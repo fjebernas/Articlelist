f78176746acb9b9fad9e54ede1a26f682e59c47d
francisjosephironman <francis.3.6.joseph@gmail.com>
2023-02-15 22:54:40 +0800

    updated article model

--- articlelist/src/main/java/com/fjebernas/articlelist/model/Article.java
+++ articlelist/src/main/java/com/fjebernas/articlelist/model/Article.java
@@ -1,7 +1,16 @@
 package com.fjebernas.articlelist.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.Instant;
+
+@NoArgsConstructor
+@Setter
+@Getter
 @Entity
 @Table(name = "articles")
 public class Article {
@@ -9,8 +18,8 @@ public class Article {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(name = "description", nullable = false)
-  private String description;
+  @Column(name = "context", nullable = false)
+  private String context;
 
   @Column(name = "category", nullable = false)
   private String category;
@@ -18,56 +27,7 @@ public class Article {
   @Column(name = "link", nullable = false)
   private String link;
 
-  @Column(name = "keywords", nullable = true)
-  private String keywords;
-
-  public Article() {
-  }
-
-  public Article(String description, String category, String link, String keywords) {
-    this.description = description;
-    this.category = category;
-    this.link = link;
-    this.keywords = keywords;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public String getCategory() {
-    return category;
-  }
-
-  public void setCategory(String category) {
-    this.category = category;
-  }
-
-  public String getLink() {
-    return link;
-  }
-
-  public void setLink(String link) {
-    this.link = link;
-  }
-
-  public String getKeywords() {
-    return keywords;
-  }
-
-  public void setKeywords(String keywords) {
-    this.keywords = keywords;
-  }
+  @CreationTimestamp
+  @Column(name = "created_at", nullable = false)
+  private Instant createdAt;
 }
